3d555acba271809c604a0631fc064044a5f0452a
Aditya Gupta <ashishgupta9936ad@gmail.com>
2024-05-21 09:06:39 +0530

    feat: added pi code

--- pii.py
+++ pii.py
@@ -10,8 +10,49 @@ import firebase_admin
 from firebase_admin import credentials
 from google.oauth2 import service_account
 import google.auth.transport.requests
+from picamera2 import Picamera2
+import RPi.GPIO as GPIO
+import time
 
 app = Flask(__name__)
+
+# FCM notification for Video Screen to On
+def sendFcmForCallScreenNotification():
+    
+    title = "PetCall"
+    body = "Sound"
+
+    fcm_endpoint = "https://fcm.googleapis.com/v1/projects/petsync-dd38d/messages:send"
+
+    try:
+        access_token = _get_access_token()
+
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+            "Content-Type": "application/json"
+        }
+
+        payload = {
+            "message": {
+                "token": "cE6Ip-gERHG95yKpnTySP8:APA91bHhh4H3sQzv-PfkG8GLlNHgmGe8w09I0qC874K5MnF2-ECD7Oa3zIMCjgf4OxyFNrDkUFKgCDG-agDIqPjLVW4DyKrMXAyjmWzLjc-Z4aqOGeo0s_R4jHHMKpQAbvU4gIkdmllY",
+                "data": {
+                    "title": title,
+                    "body": body
+                }
+            }
+        }
+
+        response = requests.post(fcm_endpoint, headers=headers, json=payload)
+
+        if response.ok:
+            print("Notification sent successfully")
+            ultrasonicSensor()
+        else:
+            print(f"Failed to send notification. Response code: {response.text}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 
 @app.post("/petSync/raspberry/onOff")
 def onOff():
@@ -27,25 +68,13 @@ def onOff():
     
 
 def onMachine(data):
-    dogCatDetection()
+    sendFcmForCallScreenNotification()
 
 
-FCM_SERVER_KEY = "AAAAOqBZjiA:APA91bE5BugAfPwEZQKIamtS0hkC_zEr0Mg-ygmDofsDlBJXx6JDcj_-IlJZorIb1E7AcLqqenxwo386RudtlY5NFhR1a9XWFmu16kZ1xocK4-ULEhLYNRBECVoMop40kunDxBN7JVHz"
-FCM_ENDPOINT = "https://fcm.googleapis.com/fcm/send"
-NEW_FCM_ENDPOINT = "http://192.168.215.21/send"
-BACKEND_ENDPOINT = "http://localhost:5000/petSync/food"
+BACKEND_ENDPOINT = "http://192.168.1.5:5000/petSync/food"
 
 
-def createFcmPayload(animal):
-    return {
-        "to": "/topic/eV4a10hMQcWKPNRkVjqjgC:APA91bHUiipbLh8pvoq2qN3ZzyQhL-ahn1A1NQGNszPj9XGuKyziee1bJ82V8UUwvqyZwyn_6NJLeJRPPqT45sJTXKpRPTrh8MB0TirRFqqYIqtN4bQ8-vz-TKywyFTJCGYNavhDAfvr",
-        "data": {
-            "title": "Feed alert",
-            "message": animal+" has been feeded",
-            "type":"food"
-        },
-    }
-
+# Payload for FCM Feed details user
 def createPetFoodPayload(animal):
     return {
         "message": {
@@ -57,6 +86,7 @@ def createPetFoodPayload(animal):
             }
     }
 
+# FCM notification to Pet Owner for Feed details
 def sendPetFeedNotification(animal):
     fcm_endpoint = "https://fcm.googleapis.com/v1/projects/petsync-dd38d/messages:send"
 
@@ -79,23 +109,7 @@ def sendPetFeedNotification(animal):
         print(f"An error occurred: {e}")
 
 
-
-def sendNotification(animal):
-
-    payload = createFcmPayload(animal)
-    headers = {
-        "Authorization": f"key={FCM_SERVER_KEY}",
-        "Content-Type": "application/json",
-    }
-
-    response = requests.post(FCM_ENDPOINT, json=payload, headers=headers)
-
-    if response.status_code == 200:
-        print("success")
-    else:
-        print(response.text)
-
-
+# Payload data for Save Pet Feed Spring boot
 def createFoodPayload(animal):
     return {
         "pet": animal,
@@ -103,6 +117,7 @@ def createFoodPayload(animal):
     }
 
 
+# Save Pet Feed Details into Spring boot
 def saveFoodDetails(animal):
     foodPayload = createFoodPayload(animal)
     headers = {
@@ -120,13 +135,8 @@ def saveFoodDetails(animal):
 
 
 
-
+# PetDetection Model Yolo
 def dogCatDetection():
-    
-    # start webcam
-    cap = cv2.VideoCapture(0)
-    cap.set(3, 640)
-    cap.set(4, 480)
 
     # model
     model = YOLO("yolo-Weights/yolov8n.pt")
@@ -146,12 +156,22 @@ def dogCatDetection():
 
     cooldown_duration = 60
     last_notification_time = 0
+    
+    # Camera
+    picam2 = Picamera2()
+    picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+    picam2.start()
+    
+    checkClose = True
 
-    while True:
+    while checkClose:
 
         current_time = time.time()
     
-        success,img = cap.read()
+        img = picam2.capture_array("main")
+        
+        img = cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
+        
         results = model(img, stream=True)
 
         # coordinates
@@ -186,23 +206,27 @@ def dogCatDetection():
                     if current_time - last_notification_time >= cooldown_duration:
                         sendPetFeedNotification(classNames[cls])
                         saveFoodDetails(classNames[cls])
-                        send_fcm_notification()
+                        sendFcmForVideoCallScreenNotification()
+                        if(cls == 15):
+                            foodDespenserCat()
+                        if(cls == 16):
+                            foodDespenserDog()
                         last_notification_time = current_time
+                        checkClose = False
 
 
         cv2.imshow('Webcam', img)
         if cv2.waitKey(1) == ord('q'):
             break
 
-    cap.release()
+    picam2.close()
     cv2.destroyAllWindows()
 
 
 
-
-def send_fcm_notification():
-
-    # Example usage
+# FCM notification for Video Screen to On
+def sendFcmForVideoCallScreenNotification():
+    
     title = "Notification Title"
     body = "Notification Body"
 
@@ -237,6 +261,7 @@ def send_fcm_notification():
         print(f"An error occurred: {e}")
 
 
+# Get Access Token for FCM
 def _get_access_token():
     
     SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
@@ -245,48 +270,139 @@ def _get_access_token():
     request = google.auth.transport.requests.Request()
     credentials.refresh(request)
     return credentials.token
-
-
-# FCM notification for Video Screen to On
-def sendFcmForVideoCallScreenNotification():
     
-    title = "PetCall"
-    body = "Sound"
+    
+def foodDespenserCat():
+    print("cattttttt is detected")
+    control = [5,5.5,6,6.5,7,7.5,8,8.5,9,9.5,10]
 
-    fcm_endpoint = "https://fcm.googleapis.com/v1/projects/petsync-dd38d/messages:send"
+    servo = 22
 
+    GPIO.setmode(GPIO.BOARD)
+
+    GPIO.setup(servo,GPIO.OUT)
+# in servo motor,
+# 1ms pulse for 0 degree (LEFT)
+# 1.5ms pulse for 90 degree (MIDDLE)
+# 2ms pulse for 180 degree (RIGHT)
+
+# so for 50hz, one frequency is 20ms
+# duty cycle for 0 degree = (1/20)*100 = 5%
+# duty cycle for 90 degree = (1.5/20)*100 = 7.5%
+# duty cycle for 180 degree = (2/20)*100 = 10%
+
+    p=GPIO.PWM(servo,50)# 50hz frequency
+
+    p.start(2.5)# starting duty cycle ( it set the servo to 0 degree )
     try:
-        access_token = _get_access_token()
+        for x in range(11):
+            p.ChangeDutyCycle(control[x])
+            time.sleep(0.03)
+            print(x)
+           
+        for x in range(9,0,-1):
+            p.ChangeDutyCycle(control[x])
+            time.sleep(0.03)
+            print(x)
+        
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        
 
-        headers = {
-            "Authorization": f"Bearer {access_token}",
-            "Content-Type": "application/json"
-        }
+def foodDespenserDog():
+    print("Dogggg is detected")
+    control = [5,5.5,6,6.5,7,7.5,8,8.5,9,9.5,10]
 
-        payload = {
-            "message": {
-                "token": "cE6Ip-gERHG95yKpnTySP8:APA91bHhh4H3sQzv-PfkG8GLlNHgmGe8w09I0qC874K5MnF2-ECD7Oa3zIMCjgf4OxyFNrDkUFKgCDG-agDIqPjLVW4DyKrMXAyjmWzLjc-Z4aqOGeo0s_R4jHHMKpQAbvU4gIkdmllY",
-                "data": {
-                    "title": title,
-                    "body": body
-                }
-            }
-        }
+    servo = 18
 
-        response = requests.post(fcm_endpoint, headers=headers, json=payload)
+    GPIO.setmode(GPIO.BOARD)
 
-        if response.ok:
-            print("Notification sent successfully")
-        else:
-            print(f"Failed to send notification. Response code: {response.text}")
+    GPIO.setup(servo,GPIO.OUT)
+# in servo motor,
+# 1ms pulse for 0 degree (LEFT)
+# 1.5ms pulse for 90 degree (MIDDLE)
+# 2ms pulse for 180 degree (RIGHT)
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
+# so for 50hz, one frequency is 20ms
+# duty cycle for 0 degree = (1/20)*100 = 5%
+# duty cycle for 90 degree = (1.5/20)*100 = 7.5%
+# duty cycle for 180 degree = (2/20)*100 = 10%
+
+    p=GPIO.PWM(servo,50)# 50hz frequency
+
+    p.start(2.5)# starting duty cycle ( it set the servo to 0 degree )
+    try:
+        for x in range(11):
+            p.ChangeDutyCycle(control[x])
+            time.sleep(0.03)
+            print(x)
+           
+        for x in range(9,0,-1):
+            p.ChangeDutyCycle(control[x])
+            time.sleep(0.03)
+            print(x)
+        
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+
+def ultrasonicSensor():
+    #GPIO Mode (BOARD / BCM)
+    GPIO.setmode(GPIO.BCM)
+ 
+    #set GPIO Pins
+    GPIO_TRIGGER = 4
+    GPIO_ECHO = 17
+ 
+    #set GPIO direction (IN / OUT)
+    GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+    GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+    def distance():
+        # set Trigger to HIGH
+        GPIO.output(GPIO_TRIGGER, True)
+ 
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.000001)
+        GPIO.output(GPIO_TRIGGER, False)
+ 
+        StartTime = time.time()
+        StopTime = time.time()
+    
+        # save StartTime
+        while GPIO.input(GPIO_ECHO) == 0:
+            StartTime = time.time()
+ 
+        # save time of arrival
+        while GPIO.input(GPIO_ECHO) == 1:
+            StopTime = time.time()
 
 
+ 
+        # time difference between start and arrival
+        TimeElapsed = StopTime - StartTime
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = (TimeElapsed * 34600) / 2
+        return distance
+    
+    try:
+        while True:
+            dist = distance()
+            print ("Measured Distance = %.1f cm" % dist)
+            if(dist<=30):
+                GPIO.cleanup()
+                dogCatDetection()
+                break
+            time.sleep(1)
+        GPIO.cleanup()
+ 
+        # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
 
 
+# Main function
 if __name__ == '__main__':
-    send_fcm_notification()
-    # dogCatDetection()
-    # app.run(host='192.168.151.21',port =8000, debug = True)
+    #ultrasonicSensor()
+    app.run(host='192.168.1.6',port =8000, debug = True)
